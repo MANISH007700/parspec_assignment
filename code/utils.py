23b559ab789ffbf62de9a4cf481944fbcdcfba54
MANISH007700 <monalmonal007@gmail.com>
2023-11-30 09:25:05 +0530

    Added code to extract text from all pages using PyMuPDF and Fitz and save it

--- code/utils.py
+++ code/utils.py
@@ -1,5 +1,6 @@
 import os 
 import requests 
+import sys, pathlib, fitz
 import time 
 import pandas as pd 
 from loguru import logger
@@ -26,17 +27,37 @@ def download_pdf_from_url(file_id: str, file_url: str, dir_name: str) -> None:
         print(f"PDF-ID {file_id} downloaded successfully.")
     else:
         print(f"Failed to download PDF-ID {file_id}. Status code: {response.status_code}")
-
-
     
+
+def extract_text_from_pdf(pdf_dir: str, pdf_file_name: str, save_path_dir: str) -> None:
+    """
+        Args:
+            pdf_dir : dir where the pdf is stored
+            pdf_file_name : name of the pdf file 
+            save_path_dir : dir where the ocr text of the pdf will be saved
+        Return:
+            None
+    """
+    
+    full_path = os.path.join(pdf_dir, pdf_file_name)  # get full path
+
+    with fitz.open(full_path) as doc:  # open document
+        text = chr(12).join([page.get_text() for page in doc])
+    logger.info(f"All text extracted, {pdf_file_name},  saving them....")
+
+    # write as a binary file to support non-ASCII characters
+    save_text_path = os.path.join(save_path_dir, pdf_file_name.split(".")[0] + ".txt")
+    pathlib.Path(save_text_path).write_bytes(text.encode())
 
 
 if __name__ == "__main__":
 
+    
     # read the training.csv file to iterate on file_url 
     training_df = pd.read_csv(r"/mnt/g/Users/Manish/Desktop/interview_prep/parspec/parspec_assignment/data/parspec_train_data - parspec_train_data.csv")
     testing_df = pd.read_csv(r"/mnt/g/Users/Manish/Desktop/interview_prep/parspec/parspec_assignment/data/parspec_test_data - parspec_test_data.csv")
 
+    # format = {"dataset_category": ["datset_category_df", "dataset_category_dir"]}
     all_dataframe = {"train": [training_df, "train/"], "test": [testing_df, "test/"] }
     
     for set, item in all_dataframe.items():
@@ -58,3 +79,22 @@ if __name__ == "__main__":
         end_time = time.time()  # end the time
 
         logger.info(f"Time taken to load and download {set} set --> {end_time - start_time}")
+
+    
+    
+    # iterate through both train and test pdf directory and extract text out of it and save it
+    # format = {"dataset_category": ["datset_category_pdf_path", "dataset_category_save_path"]}
+    pdf_paths = {
+        "train": ["/mnt/g/Users/Manish/Desktop/interview_prep/parspec/parspec_assignment/download_pdf/train", "/mnt/g/Users/Manish/Desktop/interview_prep/parspec/parspec_assignment/all_ocr_pdf/train" ],
+        "test": ["/mnt/g/Users/Manish/Desktop/interview_prep/parspec/parspec_assignment/download_pdf/test", "/mnt/g/Users/Manish/Desktop/interview_prep/parspec/parspec_assignment/all_ocr_pdf/test" ]
+    }
+
+    start_time = time.time()  # start time 
+    for dataset_category, stored_path_info in pdf_paths.items():
+        logger.info(f"Extraction started for category {dataset_category}......")
+        for f in os.listdir(stored_path_info[0]):
+
+            extract_text_from_pdf(stored_path_info[0], f, stored_path_info[1])
+    end_time = time.time()  # end time
+
+    logger.info(f"Time taken to extract text from both train and test set is --> {end_time - start_time} seconds..") 
