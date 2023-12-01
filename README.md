d06939f54d73efdf05adcede4354d4a18a5e99b7
MANISH007700 <monalmonal007@gmail.com>
2023-12-01 22:20:41 +0530

    Added code to save the extracted text into a df.csv files

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ This repo contains code and info for parspec assignment for DataScientist Role
 
 - [x] : Download the pdf file from the URL and save them in directory [ ~ 1hrs ] - time taken to download, not for code [ start date - 30/11/23 ]
 - [x] : Extract text from the pdf files and save them in a .txt file [ ~ 30 mins ] - time taken to extract text, not for code [ start date - 30/11/23 ]
-- [ ] : Clean the extracted text, preprocessing, checking out data and making clean pipeline
+- [x] : Convert the extracted text into a csv file [ for both train and test ]. This csv will be used for finetuning model
 - [ ] : Try out Debertav3 model from MSFT for text classification
 - [ ] : Try out MultiModality Model [ Text + Images ] to be used for context
 - [ ] : Benchmarking the scores on test data 
--- code/utils.py
+++ code/utils.py
@@ -3,11 +3,13 @@ import requests
 import sys, pathlib, fitz
 import time 
 import pandas as pd 
+from tqdm import tqdm
+from pandas import DataFrame
 from loguru import logger
 
-### function to download the pdf using file_url ### 
+
 def download_pdf_from_url(file_id: str, file_url: str, dir_name: str) -> None:
-    """
+    """ function to download the pdf using file_url  
         Args: 
             file_id : id of the file
             file_url : URL where the file is hosted 
@@ -30,7 +32,7 @@ def download_pdf_from_url(file_id: str, file_url: str, dir_name: str) -> None:
     
 
 def extract_text_from_pdf(pdf_dir: str, pdf_file_name: str, save_path_dir: str) -> None:
-    """
+    """ function to extract text from the pdf file 
         Args:
             pdf_dir : dir where the pdf is stored
             pdf_file_name : name of the pdf file 
@@ -50,6 +52,54 @@ def extract_text_from_pdf(pdf_dir: str, pdf_file_name: str, save_path_dir: str) 
     pathlib.Path(save_text_path).write_bytes(text.encode())
 
 
+def convert_text_file_into_dataframe(file_dir: str, training_df: DataFrame) -> DataFrame: 
+    """ convert the input text file into a dataframe column 
+        Args:
+            file_dir : the dir of the text file which contains all the raw OCR text of the pdf
+            training_df : the training df which has the info of the pdf and it's respective category
+        Return:
+            DataFrame : A pandas dataframe which has inp text and target as lighting / no lighting
+    """
+
+    # Step 0 : Create a target map 
+    target_map = {"Yes": 1, "No": 0}
+    # Step 1: Create an empty list which will later be converted into df
+    training_data = {
+        "input_id": [],
+        "input_text": [], 
+        "target": []
+    }
+
+    # Step 2 : Iterate through the input text directory
+    for filename in tqdm(os.listdir(file_dir)):
+        if filename.endswith('.txt'):
+            file_path = os.path.join(file_dir, filename)
+
+            # Step 3: Read the content of each text file
+            with open(file_path, 'r') as file:
+                text_content = file.read()
+
+            clean_text_content = text_content.replace("\n", " ").replace("•", "")
+
+            # breakpoint()
+            # Step 4 : Find the target category from the training df 
+            file_id = filename.split(".")[0]
+            target_row = training_df[training_df['ID'] == file_id]
+            # print(target_row)
+            if len(target_row) != 0:
+                # target_value = target_map[target_row['Is lighting product?'].values[0]]   # mapping the category into 0 and 1  [ do this only for training data ]
+                target_value = target_row['Is lighting product?'].values[0]  # testing data is already in 0 and 1
+            
+                # Step 5 : Add the clean content and target value in the dict
+                
+                training_data['input_id'].append(file_id)
+                training_data['input_text'].append(clean_text_content)
+                training_data['target'].append(target_value)
+
+    # Step 6 : Create the dataframe and save it
+    clean_train_df = pd.DataFrame.from_dict(training_data) 
+    clean_train_df.to_csv("testing_df_info.csv", index=False)
+
 if __name__ == "__main__":
 
     
@@ -57,6 +107,7 @@ if __name__ == "__main__":
     training_df = pd.read_csv(r"/mnt/g/Users/Manish/Desktop/interview_prep/parspec/parspec_assignment/data/parspec_train_data - parspec_train_data.csv")
     testing_df = pd.read_csv(r"/mnt/g/Users/Manish/Desktop/interview_prep/parspec/parspec_assignment/data/parspec_test_data - parspec_test_data.csv")
 
+    '''
     # format = {"dataset_category": ["datset_category_df", "dataset_category_dir"]}
     all_dataframe = {"train": [training_df, "train/"], "test": [testing_df, "test/"] }
     
@@ -98,3 +149,9 @@ if __name__ == "__main__":
     end_time = time.time()  # end time
 
     logger.info(f"Time taken to extract text from both train and test set is --> {end_time - start_time} seconds..") 
+
+    '''
+    # convert every text file into df and add the target col too
+    convert_text_file_into_dataframe( "/mnt/g/Users/Manish/Desktop/interview_prep/parspec/parspec_assignment/all_ocr_pdf/train", training_df)    # training
+    convert_text_file_into_dataframe("/mnt/g/Users/Manish/Desktop/interview_prep/parspec/parspec_assignment/all_ocr_pdf/test", testing_df)  # training
+    
